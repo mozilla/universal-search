59935e4b7a90f7a46c6882d0747aee24c580559f
Jared Hirsch <ohai@6a68.net>
2016-02-10 17:42:36 -0800

    Add window watcher code & call placeholder function on window load
    Fixes #4.

--- bootstrap.js
+++ bootstrap.js
@@ -2,11 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Cu = Components.utils;
+const {utils: Cu} = Components;
 
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyModuleGetter(this, 'UniversalSearch',
-  'chrome://usearch-lib/universal-search.js');
+  'chrome://usearch-lib/content/universal-search.js');
 
 function startup(data, reason) {
   UniversalSearch.load();  
